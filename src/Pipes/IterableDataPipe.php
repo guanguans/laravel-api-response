7d544065576c6f4e08197b550984b65c571f1e09
guanguans <ityaozm@gmail.com>
2024-11-04 15:09:41 +0800

    feat(pipes): Add IterableDataPipe for processing iterable data
    - Introduce IterableDataPipe class to handle iterable structures
    - Implement handle method to convert iterables into arrays
    - Use WithPipeArgs trait for argument handling
    - Ensure compatibility with Laravel's JsonResponse

---
+++ src/Pipes/IterableDataPipe.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2024 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/laravel-api-response
+ */
+
+namespace Guanguans\LaravelApiResponse\Pipes;
+
+use Guanguans\LaravelApiResponse\Support\Traits\WithPipeArgs;
+use Illuminate\Http\JsonResponse;
+
+class IterableDataPipe
+{
+    use WithPipeArgs;
+
+    /**
+     * @noinspection RedundantDocCommentTagInspection
+     *
+     * @param \Closure(array): \Illuminate\Http\JsonResponse $next
+     * @param  array{
+     *  status: bool,
+     *  code: int,
+     *  message: string,
+     *  data: mixed,
+     *  error: ?array,
+     * }  $structure
+     */
+    public function handle(array $structure, \Closure $next): JsonResponse
+    {
+        $structure['data'] = $this->dataFor($structure['data']);
+
+        return $next($structure);
+    }
+
+    /**
+     * @param mixed $data
+     *
+     * @return mixed
+     */
+    private function dataFor($data)
+    {
+        return is_iterable($data) ? iterator_to_array($data) : $data;
+    }
+}
