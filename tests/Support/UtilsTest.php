63be8065c094efc4c3a998a78e340d1359eb7c4c
guanguans <ityaozm@gmail.com>
2025-02-25 11:38:48 +0800

    ci(workflows, composer): Update Laravel versions and scripts
    - Add Laravel 12 support in GitHub Actions workflow.
    - Enable testing with Laravel 12 across different PHP versions.
    - Update composer scripts to include new tasks for pest coverage and testing.
    - Introduce YAML linting as part of the CI process.

--- tests/Support/UtilsTest.php
+++ tests/Support/UtilsTest.php
@@ -23,27 +23,27 @@ use Symfony\Component\HttpFoundation\Response;
 it('can return status code', function (int $code, int $statusCode): void {
     expect(Utils::statusCodeFor($code))->toBe($statusCode);
 })->group(__DIR__, __FILE__)->with([
-    ['code' => -99, 'status_code' => -99],
-    ['code' => -999999, 'status_code' => -99],
-    ['code' => 0, 'status_code' => 0],
-    ['code' => 99, 'status_code' => 99],
-    ['code' => 999999, 'status_code' => 999],
-    ['code' => Response::HTTP_CONTINUE, 'status_code' => Response::HTTP_CONTINUE],
-    ['code' => 100000, 'status_code' => Response::HTTP_CONTINUE],
-    ['code' => 600, 'status_code' => 600],
-    ['code' => 600000, 'status_code' => 600],
+    ['code' => -99, 'statusCode' => -99],
+    ['code' => -999999, 'statusCode' => -99],
+    ['code' => 0, 'statusCode' => 0],
+    ['code' => 99, 'statusCode' => 99],
+    ['code' => 999999, 'statusCode' => 999],
+    ['code' => Response::HTTP_CONTINUE, 'statusCode' => Response::HTTP_CONTINUE],
+    ['code' => 100000, 'statusCode' => Response::HTTP_CONTINUE],
+    ['code' => 600, 'statusCode' => 600],
+    ['code' => 600000, 'statusCode' => 600],
 ]);
 
 it('can validate code', function (int $code, bool $isValid): void {
     expect(Utils::isValidCode($code))->toBe($isValid);
 })->group(__DIR__, __FILE__)->with([
-    ['code' => -99, 'is_valid' => false],
-    ['code' => -999999, 'is_valid' => false],
-    ['code' => 0, 'is_valid' => false],
-    ['code' => 99, 'is_valid' => false],
-    ['code' => 999999, 'is_valid' => false],
-    ['code' => Response::HTTP_CONTINUE, 'is_valid' => true],
-    ['code' => 100000, 'is_valid' => true],
-    ['code' => 600, 'is_valid' => false],
-    ['code' => 600000, 'is_valid' => false],
+    ['code' => -99, 'isValid' => false],
+    ['code' => -999999, 'isValid' => false],
+    ['code' => 0, 'isValid' => false],
+    ['code' => 99, 'isValid' => false],
+    ['code' => 999999, 'isValid' => false],
+    ['code' => Response::HTTP_CONTINUE, 'isValid' => true],
+    ['code' => 100000, 'isValid' => true],
+    ['code' => 600, 'isValid' => false],
+    ['code' => 600000, 'isValid' => false],
 ]);
